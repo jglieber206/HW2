f8a066bce2a8e4281a77f8ae896923741ea8e7e6
jglieber206 <jglieber@gmail.com>
2015-02-13 13:47:58 -0700

    Update SlidingPuzzleState.cpp

--- HW2/SlidingPuzzleState.cpp
+++ HW2/SlidingPuzzleState.cpp
@@ -9,53 +9,68 @@
 #include "SlidingPuzzleState.h"
 #include <cstdint>
 #include <iostream>
+#include "LList.h"
 
 using namespace std;
 
-int board[12];
-
 SlidingPuzzleState::SlidingPuzzleState(const int tiles[12])
 {
-//    SlidingPuzzleState *puzzleState = new SlidingPuzzleState(exampleTiles);
-    board = tiles[12];
+    bool isSet[12] = {false,false,false,false,false,false,false,false,false,false,false,false};
+    bool isDuplicate = false;
+    for(int i = 0; i < 12; i++) {
+        if(tiles[i] >= 0 && tiles[i] < 12) {    //check to make sure range is correct
+            if(!isSet[tiles[i]]) {              //check if there are duplicates
+                isSet[tiles[i]] = true;         //set value as used already
+                myBoard.tileValue[i] = tiles[i];
+            }
+            else {
+                cerr << "Duplicate value!" << endl;
+                isDuplicate = true;
+                break;
+            }
+        }
+    }
+    if(!isDuplicate) {
+        SlidingPuzzleState *puzzleState = new SlidingPuzzleState(tiles);
+    }
 }
 
-//LList<int> GetMoves()
-//{
-//    
-//}
 
-bool ApplyMove(int move)
-{
+void SlidingPuzzleState::GetMoves(LList<int> &l) {
+    
+}
+bool SlidingPuzzleState::ApplyMove(int move){
     
 }
 
-bool UndoMove(int move)
-{
+bool SlidingPuzzleState::UndoMove(int move){
     
 }
 
-int GetTileInSquare(int row, int col)
-{
+int SlidingPuzzleState::GetTileInSquare(int row, int col){
+    if(row >=0 && row < 3 && col >= 0 && col < 4) {
+        return myBoard.tileValue[row*4+col];
+    }
+    return -1;
+}
+
+void SlidingPuzzleState::GetEmptySquare(int &row, int &col){
+    for(int i = 0; i < 12; i++) {
+        if(myBoard.tileValue[i] == 0){
+            col = i%4;
+            row = (int)i/4;
+        }
+    }
+}
+
+void SlidingPuzzleState::Print(){
     
 }
 
-void GetEmptySquare(int &row, int &col)
-{
+SlidingPuzzleState *SlidingPuzzleState::Clone(){
     
 }
 
-void Print()
-{
-    
-}
-
-SlidingSquarePuzzle *Clone()
-{
-    
-}
-
-bool IsSolution()
-{
+bool IsSolution(){
     
 }
